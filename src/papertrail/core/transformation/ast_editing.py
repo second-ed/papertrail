7f9e12465384931569b3b18c28fd88486af4cb29
ed cuss <edcussmusic@gmail.com>
2026-02-01 20:31:45 +0000

    fix: slightly more robust docstring editing

--- src/papertrail/core/transformation/ast_editing.py
+++ src/papertrail/core/transformation/ast_editing.py
@@ -1,47 +1,85 @@
 import ast
+import re
+from typing import Self
+
+import attrs
 
 
 def update_function_docstrings(
     code: str,
     examples: dict[str, str],
 ) -> str:
-    lines = code.splitlines(keepends=True)
+    replacements = create_replacements(code, examples)
+    return replace_docstrings(code, replacements)
+
+
+@attrs.define(frozen=True)
+class DocString:
+    doc: str
+    start_line: int
+    end_line: int
+    indent: str = "    "
+
+    @classmethod
+    def from_doc_node(cls, node: ast.AST) -> Self:
+        return cls(node.value.value.rstrip(), node.lineno - 1, node.end_lineno)
+
+    @classmethod
+    def from_non_doc_node(cls, node: ast.AST) -> Self:
+        start = node.lineno - 1
+        return cls("", start, start)
+
+    def with_example(self, example: str) -> Self:
+        example = "".join(
+            [
+                f"{self.indent}{line}" if line.strip() else line
+                for line in example.splitlines(keepends=True)
+            ]
+        )
+        pattern = r"(Papertrail examples:\n.*?\n\s*::)"
+        replaced_doc, n = re.subn(pattern, example, self.doc)
+
+        new_doc = replaced_doc if n else f"{self.doc}\n\n{example}"
+        return DocString(new_doc.strip(), self.start_line, self.end_line)
+
+    def to_doc(self) -> str:
+        return f'{self.indent}"""{self.doc.strip()}\n{self.indent}"""\n'
+
+
+def create_replacements(code: str, examples: dict[str, str]) -> list[DocString]:
     tree = ast.parse(code)
 
-    replacements: list[tuple[int, int, str]] = []
+    replacements = []
 
     for node in ast.walk(tree):
-        if is_fn_with_body(node) or not (example := examples.get(node.name, "")):
+        if not _is_fn_with_body(node) or not (example := examples.get(node.name, "")):
             continue
 
         first = node.body[0]
-        start = first.lineno - 1
-        indent = " " * (first.col_offset)
 
-        if is_docstring(first):
-            new_doc = f"{first.value.value.rstrip()}\n\n{example}"
-            end_line = first.end_lineno
+        if _is_docstring(first):
+            docstring = DocString.from_doc_node(first)
         else:
-            new_doc = example
-            end_line = start
+            docstring = DocString.from_non_doc_node(first)
 
-        new_docstring = f'"""{new_doc}\n"""\n\n'
-        new_docstring = "\n".join(
-            [f"{indent}{line}" if line.strip() else line for line in new_docstring.splitlines()]
-        )
-        replacements.append((start, end_line, new_docstring))
+        replacements.append(docstring.with_example(example))
 
-    for start, end, text in reversed(replacements):
-        lines[start:end] = [text]
+    return replacements
+
+
+def replace_docstrings(code: str, replacements: list[DocString]) -> str:
+    lines = code.splitlines(keepends=True)
+    for doc in reversed(replacements):
+        lines[doc.start_line : doc.end_line] = [doc.to_doc()]
 
     return "".join(lines)
 
 
-def is_fn_with_body(node: ast.AST) -> bool:
-    return not isinstance(node, ast.FunctionDef) or not node.body
+def _is_fn_with_body(node: ast.AST) -> bool:
+    return isinstance(node, ast.FunctionDef) and node.body
 
 
-def is_docstring(node: ast.AST) -> bool:
+def _is_docstring(node: ast.AST) -> bool:
     return (
         isinstance(node, ast.Expr)
         and isinstance(node.value, ast.Constant)
